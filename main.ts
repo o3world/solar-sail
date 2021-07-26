96b4adb86d5fe6935b91e0ac47e2fc47ce1672ec
Khang Nguyen <khangnguyen@Khangs-MacBook-Pro-2.local>
2021-07-26 14:48:31 -0400

    Made a module

--- main.ts
+++ main.ts
@@ -1,28 +1,28 @@
 import HubSpotClient from './hubspotClient.ts';
 
-const args = Deno.args;
+async function solarSailCli(): Promise<void> {
+  const args = Deno.args;
+  const client = new HubSpotClient();
 
-const client = new HubSpotClient();
+  for(const arg of args) {
 
-for(const arg of args) {
-
-  switch(arg) {
-    case 'pages':
-      client.syncPages('content/api/v2/pages');
-      break;
-    case 'hubdb':
-      client.syncHubDb('hubdb/api/v2/tables');
-      break;
-    case 'blogs':
-      client.syncBlogPosts('content/api/v2/blog-posts');
-      break;
-    case 'deleteBlogs':
-      client.deleteBlogs();
-      break;
+    switch(arg) {
+      case 'pages':
+        client.syncPages('content/api/v2/pages');
+        break;
+      case 'hubdb':
+        client.syncHubDb('hubdb/api/v2/tables');
+        break;
+      case 'blogs':
+        client.syncBlogPosts('content/api/v2/blog-posts');
+        break;
+      case 'deleteBlogs':
+        client.deleteBlogs();
+        break;
+    }
   }
-
 }
 
-
-
-// const json = client.syncPages('content/api/v2/pages');
+if (import.meta.main) {
+  solarSailCli();
+}
